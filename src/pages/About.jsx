4492c64cebfbabf2be5ef2a243be6175a52cabf6
Joshua <benasjoshuac@gmail.com>
2024-10-26 00:23:30 +0800

    adjust responsive view on About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,22 +5,22 @@ const About = () => {
   return (
     <div className='container mx-auto min-h-screen items-center'>
       <div className='py-4 border-b'><h1 className='text-center text-2xl font-bold'>About me</h1></div>
-      <div className='flex flex-col gap-4 p-8'>
+      <div className='flex flex-col gap-6 md:gap-4 p-2 md:p-8'>
         <div className='about flex flex-col md:flex-row justify-center h-auto px-4 py-4 gap-4 mb-4'>
           <div className='about_img flex-1 flex justify-center'>
             <img src="/assets/avatar-profile.png" alt="avatar_profile" className='w-1/2 md:3/4 rounded-full max-w-xs border-l-8 border-blue-200 object-cover' />
           </div>
           <div className='about_context flex-1 place-self-center'>
-            <p className='italic text-center tracking-wider text-gray-300 text-sm md:text-lg font-serif'> " I have a diverse background in tech and creativity, starting as an IT tech support specialist in an advertising company, where I honed my troubleshooting skills and gained insight into how technology supports business functions. I also worked as a video editor for a vlogger, where I developed an eye for detail and creativity. Currently, I’m focused on transitioning to web development, specifically the MERN stack, and have been deepening my expertise in React.js to build dynamic and user-friendly web applications."</p>
+            <p className='italic text-center tracking-wider text-gray-300 text-xs w-[250px] sm:w-full md:text-lg font-serif'> " I have a diverse background in tech and creativity, starting as an IT tech support specialist in an advertising company, where I honed my troubleshooting skills and gained insight into how technology supports business functions. I also worked as a video editor for a vlogger, where I developed an eye for detail and creativity. Currently, I’m focused on transitioning to web development, specifically the MERN stack, and have been deepening my expertise in React.js to build dynamic and user-friendly web applications."</p>
           </div>
         </div>
-        <div className='skills bg-stone-900 rounded-md p-8'>
-          <div className='skills_title'><h1 className='text-center text-lg md:text-xl lg:text-2xl font-bold'>Skills</h1></div>
+        <div className='skills bg-stone-900 rounded-md xs:p-2 md:p-8'>
+          <div className='skills_title'><h1 className='text-center text-2xl font-bold'>Skills</h1></div>
           <div className='skills_context'>
             <div className="tech_stack flex justify-center p-2 md:p-6 lg:p-8 gap-8 flex-wrap ">
               <div className="front_end w-64 h-64 flex flex-col justify-center border-2 border-gray-200 rounded-full bg-slate-200 bg-opacity-10 p-4 items-center text-center">
-                <h2 className='text-2xl font-bold'>Front End</h2>
-                <ul>
+                <h2 className='text-xl md:text-2xl font-bold'>Front End</h2>
+                <ul className="text-xs md:text-base">
                   <li>HTML</li>
                   <li>CSS</li>
                   <li>JAVASCRIPT</li>
@@ -29,8 +29,8 @@ const About = () => {
                 </ul>
               </div>
               <div className="backend w-64 h-64 flex flex-col justify-center border-2 border-gray-200 rounded-full bg-slate-200 bg-opacity-10 p-4 items-center text-center">
-                <h2 className='text-2xl font-bold'>Back End</h2>
-                <ul>
+                <h2 className='text-xl md:text-2xl font-bold'>Back End</h2>
+                <ul className="text-xs md:text-base">
                   <li>NODE JS</li>
                   <li>EXPRESS JS</li>
                   <li>MONGO DB</li>
@@ -38,8 +38,8 @@ const About = () => {
                 </ul>
               </div>
               <div className="video_and_design w-64 h-64 flex flex-col justify-center border-2 border-gray-200 rounded-full bg-slate-200 bg-opacity-10 p-4 items-center text-center">
-                <h2 className='text-2xl font-bold'>Video and Design</h2>
-                <ul>
+                <h2 className='text-xl md:text-2xl font-bold'>Video and Design</h2>
+                <ul className="text-xs md:text-base">
                   <li>PS PHOTOSHOP</li>
                   <li>FIGMA</li>
                   <li>ADOBE PREMIERE</li>
@@ -50,36 +50,36 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className="education mt-8 px-10 py-6 rounded-md shadow-md shadow-slate-800">
+        <div className="education mt-8 px-auto md:px-10 py-6 rounded-md shadow-md shadow-slate-800">
           <div className="education_title text-center text-2xl font-bold">Education</div>
           <div className='edu p-8 flex flex-col gap-4'>
             <div className='flex justify-between'>
               <div>
-                <h1 className='text-xl font-bold'>Bachelor of Science in Information and Technology</h1>
+                <h1 className='text-md md:text-2xl font-bold'>Bachelor of Science in Information and Technology</h1>
                 <p>Access Computer College</p>
               </div>
-              <h1 className='text-xl font-bold'>2017</h1>
+              <h1 className='text-md md:text-2xl font-bold'>2017</h1>
             </div>
             <div className='flex justify-between'>
               <div>
-                <h1 className='text-xl font-bold'>Computer Programming and Hardware Servicing NCII</h1>
+                <h1 className='text-md md:text-2xl font-bold'>Computer Programming and Hardware Servicing NCII</h1>
                 <p>CLaret Tech Vocational School</p>
               </div>
-              <h1 className='text-xl font-bold'>2013</h1>
+              <h1 className='text-md md:text-2xl font-bold'>2013</h1>
             </div>
             <div className='flex justify-between'>
               <div>
-                <h1 className='text-xl font-bold'>MERN Stack Course</h1>
+                <h1 className='text-md md:text-2xl font-bold'>MERN Stack Course</h1>
                 <p>Udemy Online Course</p>
               </div>
-              <h1 className='text-xl font-bold'>2024</h1>
+              <h1 className='text-md md:text-2xl font-bold'>2024</h1>
             </div>
             <div className='flex justify-between'>
               <div>
-                <h1 className='text-xl font-bold'>C# .NET CORE API with MS SQL Course</h1>
+                <h1 className='text-md md:text-2xl font-bold'>C# .NET CORE API with MS SQL Course</h1>
                 <p>Udemy Online Course</p>
               </div>
-              <h1 className='text-xl font-bold'>2024</h1>
+              <h1 className='text-md md:text-2xl font-bold'>2024</h1>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {},
+    screens: {
+      // 'xs': "200px",
+      'sm': "640px",
+      // => @media (min-width: 640px) { ... }
+
+      'md': "768px",
+      // => @media (min-width: 768px) { ... }
+
+      'lg': "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
     boxShadow: {
       sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
@@ -23,4 +37,3 @@ export default {
   },
   plugins: [],
 };
-
