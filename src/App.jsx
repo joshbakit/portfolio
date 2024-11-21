bb1720a31dfe896624addb57f23da0875ea98f65
joshbakit <benasjoshuac@gmial.com>
2024-11-21 13:01:31 +0800

    fix some layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-Routes
-
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import NotFound from './pages/NotFound'
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Home />
       <Footer />
-    </Router>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import { FaDownload } from "react-icons/fa";
+
+import Projects from "./Projects";
+import Contact from "./Contact";
+import About from "./About";
 
 const Home = () => {
   return (
-    <section className="bg-[url('/assets/bg-hero-section.jpg')] bg-cover bg-center  text-white min-h-screen flex items-center">
-      <div className="container flex flex-col-reverse md:flex-row mx-auto px-4 py-4 sm:px-6 lg:px-8 gap-10 ">
-        <div className="context flex-1 flex flex-col justify-center">
-          <h3 className="text-2xl sm:text-3xl">hello I'm Joshua,</h3>
-          <h1 className="text-4xl sm:text-6xl lg:text-8xl font-bold uppercase">Web Developer</h1>
-          <p className="text-sm sm:text-base text-left tracking-tight subpixel-antialiased">As a beginner in web development, I’m passionate about learning and improving my skills with each new project.
-            <a href="/assets/JoshuaBenas-Resume.pdf" className="text-blue-300 underline" download>donwload resume</a>
-          </p>
+    <>
+      <section className="bg-[url('/assets/portfolio-bg.jpg')] bg-cover bg-center h-[645px] text-white container mx-auto place-content-center">
+        <div className="flex flex-col-reverse content-center  justify-center md:flex-row sm:px-6 lg:px-8">
+          <div className="context md:w-[600px] p-8 md:p-0  ">
+            <h3 className="text-base">hello I&apos;m Joshua,</h3>
+            <h1 className="text-4xl sm:text-8xl text-[#55E5A4] font-bold uppercase">
+              Web Developer
+            </h1>
+            <p className="text-base text-left tracking-tight subpixel-antialiased">
+              I build web application and passionate about learning and
+              improving my skills with each new project.
+            </p>
 
-          <div className="cta-links mt-4 text-sm flex flex-col md:flex-row gap-2 sm:gap-4">
-            <a href="/projects" className='bg-gray-400 hover:bg-gray-500  py-2 px-4 rounded-full'>See my projects</a>
-            <a href="/contact" className='bg-gray-400 hover:bg-gray-500 py-2 px-4 rounded-full'>Contact me</a>
+            <div className="cta-links mt-4 text-sm flex flex-col md:flex-row gap-2 sm:gap-4">
+              <a
+                href="#projects"
+                className="bg-[#55E5A4] text-[#000] hover:bg-gray-500  py-2 px-4 rounded-full"
+              >
+                See my projects
+              </a>
+              <a
+                href="/assets/JoshuaBenas-Resume.pdf"
+                download
+                className="bg-[#55E5A4] text-[#000] hover:bg-gray-500 py-2 px-4 rounded-full flex gap-2"
+              >
+                Download Resume
+                <FaDownload size={15} />
+              </a>
+            </div>
           </div>
         </div>
+      </section>
+      <Projects />
+      <About />
+      <Contact />
+    </>
+  );
+};
 
-        <div className="image flex-1 flex justify-center">
-          <img src="/assets/avatar-profile.png" alt="profile" className="w-3/4 h-auto rounded-full max-w-xs sm:max-w-md lg:max-w-lg border-r-8 border-blue-200" />
-        </div>
-      </div>
-    </section>
-  )
-}
-
-export default Home
+export default Home;
