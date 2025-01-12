1b1f08ef31996daeb16f84f6b4c9861278ca8271
joshua <benasjoshuac@gmail.com>
2025-01-12 14:17:45 +0800

    update contact button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,8 @@ const Contact = () => {
             whenever I can.
           </p>
           <a
-            href=""
+            href="mailto:benasjoshuac@gmail.com"
+            target="_blank"
             className="text-nowrap rounded-lg border border-indigo-800 bg-gray-700 px-5 py-3 text-lg font-bold text-white shadow-lg shadow-indigo-900 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600"
           >
             Contact Me
